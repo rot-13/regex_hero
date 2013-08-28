5723bc9cf83fafc425c55dc3ddd4be1dd01e5731
Shay Davidson <shay.h.davidson@gmail.com>
2013-08-28 17:11:31 +0300

    Initial commit - Added rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RegexHero::Application.config.secret_key_base = '5e234c5e85bc00cac7edc1f5ea1d80c7e45e7de58ee7b97f28ba789432da0a337e9e1a6911348599c3de41f7d203175a02af6085557dc422d4ac28428090f1e3'
